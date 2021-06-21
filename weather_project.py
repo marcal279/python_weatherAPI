110bd0e0320e87260a888027fe4840e1434089de
Marc Almeida <73192810+marcal279@users.noreply.github.com>
2021-06-22 03:02:27 +0530

    Update weather_project.py

--- weather_project.py
+++ weather_project.py
@@ -1,13 +1,3 @@
-'''
-TODO
-Use Open Weather API and Python to find weather of a city, display it and write to text file 'Weather Report.txt'
-
-Add foll. parameters
-1. Temperature
-2. Description
-3. Humidity
-4. Wind Speed
-'''
 import requests
 #import bs4
 from datetime import datetime
